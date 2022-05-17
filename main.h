0959d5709b70e5c309053ec2f93a89d088a3f2de
simri77 <simretmesfin97@gmail.com>
2022-05-17 11:03:49 -0700

    error message

--- main.h
+++ main.h
@@ -1,11 +1,34 @@
 #ifndef SIMPLE_SHELL
 #define SIMPLE_SHELL
 
-#include <stdio.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+
+/*string-fun*/
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+
+/*error_mess-A*/
+int create_error(char **args, int err);
+char *error_env(char **args);
+char *error_1(char **args);
+char *error_2_exit(char **args);
+char *error_2_cd(char **args);
+char *error_2_syntax(char **args);
+char *error_126(char **args);
+char *error_127(char **args);
 
 #endif
