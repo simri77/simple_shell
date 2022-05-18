1eee0ea485ba41ab37fa413151c270c550feb406
Selam25-30 <selamawitbirhanu123@gmail.com>
2022-05-18 08:29:25 -0700

    betty style

--- string-func.c
+++ string-func.c
@@ -15,15 +15,14 @@ int _strncmp(const char *s1, const char *s2, size_t n);
  */
 char *_strchr(char *s, char c)
 {
-  int index;
+	int index;
 
-  for (index = 0; s[index]; index++)
-    {
-      if (s[index] == c)
-	return (s + index);
-    }
-
-  return (NULL);
+	for (index = 0; s[index]; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return (NULL);
 }
 
 /**
@@ -36,22 +35,22 @@ char *_strchr(char *s, char c)
  */
 int _strspn(char *s, char *accept)
 {
-  int bytes = 0;
-  int index;
+	int bytes = 0;
+	int index;
 
-  while (*s)
-    {
-      for (index = 0; accept[index]; index++)
+	while (*s)
 	{
-	  if (*s == accept[index])
-	    {
-	      bytes++;
-	      break;
-	    }
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+		}
+		s++;
 	}
-      s++;
-    }
-  return (bytes);
+	return (bytes);
 }
 
 /**
@@ -65,16 +64,15 @@ int _strspn(char *s, char *accept)
  */
 int _strcmp(char *s1, char *s2)
 {
-  while (*s1 && *s2 && *s1 == *s2)
-    {
-      s1++;
-      s2++;
-    }
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-  if (*s1 != *s2)
-    return (*s1 - *s2);
-
-  return (0);
+	if (*s1 != *s2)
+		return (*s1 - *s2);
+	return (0);
 }
 
 /**
@@ -89,17 +87,17 @@ int _strcmp(char *s1, char *s2)
  */
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
-  size_t i;
+	size_t i;
 
-  for (i = 0; s1[i] && s2[i] && i < n; i++)
-    {
-      if (s1[i] > s2[i])
-	return (s1[i] - s2[i]);
-      else if (s1[i] < s2[i])
-	return (s1[i] - s2[i]);
-    }
-  if (i == n)
-    return (0);
-  else
-    return (-15);
+	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	{
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+	}
+	if (i == n)
+		return (0);
+	else
+		return (-15);
 }
